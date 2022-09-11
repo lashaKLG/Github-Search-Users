40fe0efc45b567e791b3f94ea86814cff8b66b01
lashaKLG <lasha2772@gmail.com>
2022-09-12 00:03:35 +0200

    githubUserSearch

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Profile from './components/Profile';
+
+
+function App() {
+  return (
+    <div className="App">     
+        <h1>Github user search</h1>  
+      <Profile />
+  
+    </div>
+  );
+}
+
+export default App;
